7d78d36c1ed8e5fbd35c1c96b714ba47ebccf19b
BluSkreen <jasonduran303@gmail.com>
2023-02-14 12:52:33 -0700

    connection with username. Displays usernames

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,16 @@ import './App.css'
 
 const WS_URL = 'ws://127.0.0.1:8000';
 
+function isUserEvent(message) {
+    let evt = JSON.parse(message.data);
+    return evt.type === "userevent";
+}
+
+function isDocumentEvent(message) {
+    let evt = JSON.parse(message.data);
+    return evt.type === "contentchange";
+}
+
 function App() {
     const [count, setCount] = useState(0)
     const [username, setUsername] = useState('');
@@ -24,14 +34,78 @@ function App() {
                 username,
                 type: "userevent"
             });
+            console.log("json sent");
         }
     }, [username, sendJsonMessage, readyState]);
 
     return (
         <div>
-            {username ? <p>logged in</p> : <p>not logged in</p>}
+            {username ? <EditorSection/> : <LoginSection onLogin={setUsername}/>}
         </div>
     )
+}
+
+function LoginSection({ onLogin }) {
+  const [username, setUsername] = useState('');
+  useWebSocket(WS_URL, {
+    share: true,
+    filter: () => false
+  });
+  function logInUser() {
+    if(!username.trim()) {
+      return;
+    }
+    onLogin && onLogin(username);
+  }
+
+  return (
+    <div className="">
+      <label>
+        Name:
+        <input type="text" name="username"  onInput={(e) => {
+            e.preventDefault;
+            setUsername(e.target.value)
+
+            }} className="" />
+      </label>
+          <button
+            type="button"
+            onClick={() => logInUser()}
+            className="">Join</button>
+    </div>
+  );
+}
+
+function Users() {
+    const { lastJsonMessage } = useWebSocket(WS_URL, {
+        share:true,
+        filter: isUserEvent
+    });
+    const users = Object.values(lastJsonMessage?.data.users || {});
+    return (
+        <div>
+            {users.map(user => (
+            <span key={user.username} className="mx-3">
+                {user.username}
+            </span>))}
+        </div>
+    );
+}
+
+function Document() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+function EditorSection() {
+    return (
+        <div>
+            <Users/>
+            <Document/>
+        </div>
+    );
 }
 
 export default App
--- server/server.ts
+++ server/server.ts
@@ -1,23 +1,80 @@
-import { WebSocketServer } from "ws";
-import http from "http";
+import { WebSocket, WebSocketServer } from "ws";
 import { v4 as uuidv4 } from "uuid";
+import http from "http";
 
 const port = 8000;
 const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
 
+let clients: any = {};
+
 server.listen(port, () => {
     console.log(`WebSocket server is running on port ${port}`);
 });
 
-wsServer.on("conneciton", (connection) => {
+const users: any = {};
+// The current editor content is maintained here.
+let editorContent: any = null;
+// User activity history.
+let userActivity: any = [];
 
+// Event types
+const typesDef = {
+  USER_EVENT: 'userevent',
+  CONTENT_CHANGE: 'contentchange'
+}
+
+function broadcastMessage(json: any) {
+  // We are sending the current data to all connected clients
+  const data = JSON.stringify(json);
+  for(let userId in clients) {
+    let client = clients[userId];
+    if(client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  };
+}
+
+function handleMessage(message: any, userId: any) {
+  const dataFromClient = JSON.parse(message.toString());
+  const json: any = { type: dataFromClient.type };
+  if (dataFromClient.type === typesDef.USER_EVENT) {
+    users[userId] = dataFromClient;
+    userActivity.push(`${dataFromClient.username} joined to edit the document`);
+    json.data = { users, userActivity };
+  } else if (dataFromClient.type === typesDef.CONTENT_CHANGE) {
+    editorContent = dataFromClient.content;
+    json.data = { editorContent, userActivity };
+  }
+  broadcastMessage(json);
+}
+
+function handleDisconnect(userId: any) {
+    console.log(`${userId} disconnected.`);
+    const json: any = { type: typesDef.USER_EVENT };
+    const username = users[userId]?.username || userId;
+    userActivity.push(`${username} left the document`);
+    json.data = { users, userActivity };
+    delete clients[userId];
+    delete users[userId];
+    broadcastMessage(json);
+}
+
+
+wsServer.on("connection", function(connection) {
+
+    connection.on("error", console.error);
+
+    const userId = uuidv4();
     console.log("Recieved a new connection.");
 
-    connection.on("message", (data: String) => {
-        console.log(`Recieved message from client: ${data}`);
-    })
+    clients[userId] = connection;
 
-    connection.send(`Hello, this is server.ts! Here is a uuid: ${uuidv4()}`);
+    console.log(`${userId} connected.`);
+    connection.on('message', (message: any) => handleMessage(message, userId));
+    // User disconnected
+    connection.on('close', () => handleDisconnect(userId));
+
+    connection.send(`Hello, this is server.ts!`);
 })
 
